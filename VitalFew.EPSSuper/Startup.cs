113264a023d5381085da9d7ff89634d9fa25aa14
dhanushkademel <dhanushka.demel@gmail.com>
2015-05-21 20:01:40 +1000

    Inistal addition

---
+++ VitalFew.EPSSuper/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VitalFew.EPSSuper.Startup))]
+namespace VitalFew.EPSSuper
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
